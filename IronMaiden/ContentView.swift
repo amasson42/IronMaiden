cd6c170d1fce5c601d3757e7109ec87dc80fe198
amasson42 <arthur.masson@vistory.fr>
2020-09-11 18:00:38 +0200

    Initial Commit

---
+++ IronMaiden/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  IronMaiden
+//
+//  Created by Vistory Group on 11/09/2020.
+//  Copyright © 2020 Vistory Group. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
